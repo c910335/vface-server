f72c20e5d69ee889efc2e28976cae058bb9f2038
Tatsiujin Chin <c910335@gmail.com>
2020-09-11 22:05:04 +0800

    handle disconnection properly

--- main.py
+++ main.py
@@ -10,29 +10,31 @@ connected = False
 async def handler(socket, path):
     global connected
     if connected:
-        socket.close()
+        await socket.close()
         return
     connected = True
     print(f'Connected from {socket.remote_address}')
 
     tracker = Tracker()
     calc = Calculator(tracker.width)
-    while True:
-        if tracker.update():
-            calc.update(tracker.points)
-            await socket.send(f'AngleX {calc.angle_x()}')
-            await socket.send(f'AngleY {calc.angle_y()}')
-            await socket.send(f'AngleZ {calc.angle_z()}')
-            await socket.send(f'EyeLOpen {calc.eye_l_open()}')
-            await socket.send(f'EyeROpen {calc.eye_r_open()}')
-            await socket.send(f'EyeBallX {calc.eye_ball_x()}')
-            await socket.send(f'EyeBallY {calc.eye_ball_y()}')
-            await socket.send(f'MouthOpenY {calc.mouth_open_y()}')
-            await socket.send(f'BodyAngleZ {calc.body_angle_z()}')
-            sleep(Config['DELAY'] / 1000)
-    await socket.close()
-    connected = False
-    print('Disconnect')
+    try:
+        while True:
+            if tracker.update():
+                calc.update(tracker.points)
+                await socket.send(f'AngleX {calc.angle_x()}')
+                await socket.send(f'AngleY {calc.angle_y()}')
+                await socket.send(f'AngleZ {calc.angle_z()}')
+                await socket.send(f'EyeLOpen {calc.eye_l_open()}')
+                await socket.send(f'EyeROpen {calc.eye_r_open()}')
+                await socket.send(f'EyeBallX {calc.eye_ball_x()}')
+                await socket.send(f'EyeBallY {calc.eye_ball_y()}')
+                await socket.send(f'MouthOpenY {calc.mouth_open_y()}')
+                await socket.send(f'BodyAngleZ {calc.body_angle_z()}')
+                sleep(Config['DELAY'] / 1000)
+    except websockets.exceptions.ConnectionClosedError:
+        connected = False
+        print('Disconnect')
+
 
 server = websockets.serve(handler, 'localhost', Config['PORT'])
 asyncio.get_event_loop().run_until_complete(server)
